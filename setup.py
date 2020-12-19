f6444ae491d10c0ab5e24a75c9ae1710c274f977
Francisco Jose Cotan Lopez <fjcotan@gc.axesor.com>
2020-12-19 10:42:30 +0100

    Excluding tests when creating dist files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='cloud_queue',
     version='0.0.2',
     description='An abstraction of SQS and Azure Queue Storage',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests',)),
     author='Francisco José Cotán López',
     keywords=['queue', 'cloud', 'aws', 'azure'],
     url='https://github.com/Bolargo/cloud_queue',
