8c14d7b747e11e74226acea2fddc9bd184e29d62
Francisco Jose Cotan Lopez <fjcotan@gc.axesor.com>
2020-12-26 18:19:12 +0100

    Added install requires to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,8 @@ setuptools.setup(
     author='Francisco José Cotán López',
     keywords=['queue', 'cloud', 'aws', 'azure'],
     url='https://github.com/Bolargo/cloud-queue',
-    python_requires='>=3.8'
+    python_requires='>=3.8',
+    install_requires=[
+        'boto3', 'azure-storage-queue'
+    ]
 )
